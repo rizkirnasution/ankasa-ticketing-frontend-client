ddd7742cd88f0785c7a1537b47afdb8213e8aff3
mallyaagung <Mallyaagung@gmail.com>
2022-10-25 12:58:37 +0800

    fix: get user id while booking

--- src/redux/actions/transaction.js
+++ src/redux/actions/transaction.js
@@ -1,52 +1,52 @@
 import axios from "axios";
 import {
-    GET_MYBOOKING_PENDING,
-    GET_MYBOOKING_SUCCESS,
-    GET_MYBOOKING_FAILED,
-    GET_DETAIL_BOOKING_PENDING,
-    GET_DETAIL_BOOKING_SUCCESS,
-    GET_DETAIL_BOOKING_FAILED
+  GET_MYBOOKING_PENDING,
+  GET_MYBOOKING_SUCCESS,
+  GET_MYBOOKING_FAILED,
+  GET_DETAIL_BOOKING_PENDING,
+  GET_DETAIL_BOOKING_SUCCESS,
+  GET_DETAIL_BOOKING_FAILED,
 } from "./types";
 
 export const getMyBooking = (userId, navigate) => async (dispatch) => {
-    try {
-        const token = localStorage.getItem("token");
+  try {
+    const token = localStorage.getItem("token");
 
-        dispatch({
-            type: GET_MYBOOKING_PENDING,
-            payload: null,
-        });
+    dispatch({
+      type: GET_MYBOOKING_PENDING,
+      payload: null,
+    });
 
-        const res = await axios.get(
-            `${process.env.REACT_APP_API_URL}/userTransactions/${userId}`,
-            {
-                headers: { 
-                    Authorization : `Bearer ${token}`
-                 },
-                // withCredentials: true,
-            }
-        );
-        console.log("nana "+ res.data.data)
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/userTransactions/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        // withCredentials: true,
+      }
+    );
+    console.log(res.data.data);
 
-        dispatch({
-            type: GET_MYBOOKING_SUCCESS,
-            payload: res.data,
-        });
-    } catch (error) {
-        if (error.response) {
-            if (parseInt(error.response.data.code, 10) === 401) {
-                localStorage.clear();
-                return navigate("/");
-            }
+    dispatch({
+      type: GET_MYBOOKING_SUCCESS,
+      payload: res.data,
+    });
+  } catch (error) {
+    if (error.response) {
+      if (parseInt(error.response.data.code, 10) === 401) {
+        localStorage.clear();
+        return navigate("/");
+      }
 
-            error.message = error.response.data.error;
-        }
-
-        dispatch({
-            type: GET_MYBOOKING_FAILED,
-            payload: error.message,
-        });
+      error.message = error.response.data.error;
     }
+
+    dispatch({
+      type: GET_MYBOOKING_FAILED,
+      payload: error.message,
+    });
+  }
 };
 
 export const getBookingDetails = (id, navigate) => async (dispatch) => {
@@ -61,9 +61,9 @@ export const getBookingDetails = (id, navigate) => async (dispatch) => {
     const res = await axios.get(
       `${process.env.REACT_APP_API_URL}/detailTransactions/${id}`,
       {
-        headers: { 
-            Authorization : `Bearer ${token}`
-         },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
         // withCredentials: true,
       }
     );
@@ -90,41 +90,45 @@ export const getBookingDetails = (id, navigate) => async (dispatch) => {
 };
 
 export const payTicket = (id) => {
-    const token = localStorage.getItem("token")
+  const token = localStorage.getItem("token");
 
-    return new Promise((resolve, reject) => {
-        axios.put(`${process.env.REACT_APP_API_URL}/transactions/${id}/paid`, {}, {
-            headers: {
-                token: token
-            },
-            // withCredentials: true
-        })
-            .then((response) => {
-                resolve(response.data)
-            })
-            .catch((err) => {
-                reject(err)
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios
+      .put(
+        `${process.env.REACT_APP_API_URL}/transactions/${id}/paid`,
+        {},
+        {
+          headers: {
+            token: token,
+          },
+          // withCredentials: true
+        }
+      )
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
 
 export const deleteTicket = (id) => {
-    const token = localStorage.getItem("token")
+  const token = localStorage.getItem("token");
 
-    return new Promise((resolve, reject) => {
-        axios.delete(`${process.env.REACT_APP_API_URL}/transactions/${id}`, {
-            headers: {
-                token: token,
-                
-            },
-            // withCredentials: true
-        })
-            .then((response) => {
-                resolve(response.data)
-            })
-            .catch((err) => {
-                reject(err)
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}/transactions/${id}`, {
+        headers: {
+          token: token,
+        },
+        // withCredentials: true
+      })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
-
